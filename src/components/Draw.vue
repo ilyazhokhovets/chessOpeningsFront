613d052cc1c9b5224cf0242b473c861fbc1aa5e9
ilyazhokhovets <43029511+ilyazhokhovets@users.noreply.github.com>
2023-09-26 16:42:36 +0300

    tat

--- src/components/Draw.vue
+++ src/components/Draw.vue
@@ -1,13 +1,67 @@
 <template>
-$END$
+  <div>
+  <v-stage :config="configKonva">
+    <v-layer>
+
+      <v-circle :config="configCircle"></v-circle>
+      <v-text :config="text"></v-text>
+    </v-layer>
+  </v-stage>
+    <button @click="print2">delete db</button>
+<button @click="getTree">delete db</button>
+    </div>
+
 </template>
 
 <script>
 export default {
-name: "Draw"
-}
+  data() {
+    return {
+      test: 0,
+      text: {
+        text: 'te12312313xt'
+      },
+      configKonva: {
+        width: 200,
+        height: 200
+      },
+      configCircle: {
+        x: 100,
+        y: 100,
+        radius: 70,
+        stroke: "black",
+        strokeWidth: 4
+      }
+    };
+  },
+  methods:{
+    print1 (){
+
+},
+    print2(){
+      console.log(this.test)
+    },
+
+     async getTree(){
+      let data = {
+
+      }
+
+      this.listMove = await fetch(
+          `${this.$store.getters.getServerUrl}/getTree/`,
+          {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)}
+      ).then(response => response.json())
+       this.test = this.listMove
+
+
+    },
+  }
+};
+
+
 </script>
-
-<style scoped>
-
-</style>
